0fe0833d80097aeabe1b0fbecbfd6728d6d1d406
Motdep@sse <lirsitogo2021@gmail.com>
2022-06-20 13:57:20 +0000

    Include firebase

--- src/main.js
+++ src/main.js
@@ -2,15 +2,29 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase/app";
 import BootstrapVue from "bootstrap-vue";
 import "./assets/css/main.css";
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD7q0ItK89xaKyVcqZRTYooGyzTSpbuRHo",
+  authDomain: "app-parenn.firebaseapp.com",
+  databaseURL: "https://app-parenn-default-rtdb.firebaseio.com",
+  projectId: "app-parenn",
+  storageBucket: "app-parenn.appspot.com",
+  messagingSenderId: "636879638718",
+  appId: "1:636879638718:web:de12af2730a2596aed0eef",
+  measurementId: "G-V64YY80HXX"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount("#app");
